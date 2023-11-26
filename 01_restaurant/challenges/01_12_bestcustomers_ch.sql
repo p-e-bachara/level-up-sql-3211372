023bfa0de56b2ead82ec86667c52084f65e331ee
p-e-bachara <117010435+p-e-bachara@users.noreply.github.com>
2023-11-26 12:55:51 +0000

    Completed chapter Restaurant - exercises 11-12

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,16 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT 
+   COUNT(*) AS OrderCount
+  ,C.FirstName
+  ,C.LastName
+  ,C.Email
+FROM
+  Customers C 
+JOIN Orders O 
+  ON O.CustomerID = C.CustomerID
+GROUP BY C.CustomerID
+HAVING OrderCount >= 13
+ORDER BY OrderCount DESC;
